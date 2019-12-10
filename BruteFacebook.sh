746fecd47754867df0e5a3bdf732e6a1d9e8c47e
MRF1TR4/404 <58329775+FITRAKUN404@users.noreply.github.com>
2019-12-10 19:14:20 +0700

    Jangan DI RECORD KONTOL !!!!
    Jangan kau record pepek memek anying ngentot lu

---
+++ BruteFacebook.sh
@@ -0,0 +1,306 @@
+#usr/bin/bash
+#Jangan Kau Record Kontol
+#Aku capek Buat Kau Record Kontol
+#Kau Record KuSumpahin Kau gk akan pernah Sukses
+#Coded By FITRAKUN
+
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+
+echo
+echo
+echo
+sleep 1
+echo $i"JANGAN DI"$cy"RECORD"$i"YAH BEB..."
+echo
+sleep 1
+echo $me"█████████"
+echo $me"█▄█████▄█" 
+echo $me"█▼▼▼▼▼"
+echo $pu"█" $ku"CODED BY FITRAKUN" $bi"&"$me"SUBSCRIBED CHANNEL ME : JEJAK TUTORIAL ID"
+echo $pu"█▲▲▲▲▲"
+echo $pu"█████████"
+echo $pu"__██____██___"
+sleep 1
+echo $i"["$bi"•"$i"]"$me"───────────────────────────────────────────"$i"["$bi"•"$i"]"
+echo $i" |"$cy"     MR.F1TR4 CYBER DARK TEAM"$i"                |"
+echo $i" |"$me"─────────────────────────────────────────────"$i"|"
+sleep 1
+echo $i" |"$pu" AUTHOR     :"$ku" RAHMAD FITRA KUN  [MR.F1TR4]"$i"   |"
+sleep 1
+echo $i" |"$pu" FACEBOOK   :"$ku" Korek Api"$i"                      |"
+sleep 1
+echo $i" |"$pu" THANKS TO  :" $cy"MR.F1TR4 CYBER DARK TEAM"       $i"      |"
+echo $i" |"$cy"            : MR.F1TR4 CYBER TEAM"                 $i"           |"
+sleep 1
+echo $i" |"$pu" Contack WhatsApp :"$ku" 088262147113"$i"             |"
+sleep 1
+echo $i" |"$pu" Contack Gmail    :"$ku" fitrakun404@gmail.com"$i"    |"
+echo $i"["$bi"•"$i"]"$me"───────────────────────────────────────────"$i"["$bi"•"$i"]"
+echo
+sleep 1
+echo $i"────────────────────────────────    ─────"
+echo $i"|"$me" 1"$i" |"$cy" BRUTEFORCE FB PHP        "$i"|"$i"    |"$cy" B"$i" |"
+echo $i"|"$me" 2"$i" |"$cy" BRUTEFORCE FB BR1G4D3    "$i"|"$i"    |"$cy" E"$i" |"
+echo $i"|"$me" 3"$i" |"$cy" BRUTEFORCE FB iqbalz     "$i"|"$i"    |"$cy" N"$i" |"
+echo $i"|"$me" 4"$i" |"$cy" BRUTEFORCE FB MBF        "$i"|"$i"    |"$cy" G"$i" |"
+echo $i"|"$me" 5"$i" |"$cy" FACEBRUTE N1ght420       "$i"|"$i"    |"$cy" K"$i" |"
+echo $i"|"$me" 6"$i" |"$cy" BRUTEFORCE FB ( root )   "$i"|"$i"    |"$cy" E"$i" |"
+echo $i"────────────────────────────────"$i"    |"$cy" L"$i" |"
+echo $i"|"$me" 7"$i" |"$cy" YAHOO CLONING FACEBOOK   "$i"|"$i"    ─────"
+echo $i"|"$me" 8"$i" |"$cy" PROFILE GUARD FACEBOOK   "$i"|"$i"    |"$cy" F"$i" |"
+echo $i"|"$me" 9"$i" |"$cy" REPORT FACEBOOK WORK     "$i"|"$i"    |"$cy" A"$i" |"
+echo $i"|"$me"10"$i" |"$cy" AUTO REACTION FACEBOOK   "$i"|"$i"    |"$cy" C"$i" |"
+echo $i"|"$me"11"$i" |"$cy" INFORMATION FACEBOOK     "$i"|"$i"    |"$cy" E"$i" |"
+echo $i"|"$me"12"$i" |"$cy" BOT KOMEN FACEBOOK       "$i"|"$i"    |"$cy" B"$i" |"
+echo $i"|"$me"13"$i" |"$cy" AUTOLIKE FACEBOOK        "$i"|"$i"    |"$cy" O"$i" |"
+echo $i"────────────────────────────────"$i"    |"$cy" O"$i" |"
+echo $i"|"$me"14"$i" |"$cy" PHISING FACEBOOK 1       "$i"|"$i"    |"$cy" K"$i" |"
+echo $i"|"$me"15"$i" |"$cy" PHISING FACEBOOK 2       "$i"|"$i"    ─────"
+echo $i"|"$me"16"$i" |"$cy" PHISING FACEBOOK 3       "$i"|"$i"    |"$cy" F"$i" |"
+echo $i"|"$me"17"$i" |"$cy" PHISING FACEBOOK 4       "$i"|"$i"    |"$cy" I"$i" |"
+echo $i"────────────────────────────────"$i"    |"$cy" T"$i" |"
+echo $i"|"$me"18"$i" |"$cy" INSTALL BAHAN DULU COEG  "$i"|"$i"    |"$cy" R"$i" |"
+echo $i"|"$me"19"$i" |"$cy" KELUAR/EXIT PROGRAM      "$i"|"$i"    |"$cy" A"$i" |"
+echo $i"────────────────────────────────    ─────"
+echo
+sleep 1
+echo $me"┌==="$bi"["$i"MR.F1TR4"$bi"]"$me"======"$bi"["$i""Pilih Nomornya""$bi"]"
+echo $me"¦"
+read -p"└──# " pil
+
+if [ $pil = 1 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/fbbrute
+cd fbbrute
+php fb.php
+fi
+
+if [ $pil = 2 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/Fb-Cracker-v.3
+cd Fb-Cracker-v.3
+python2 crack.py
+fi
+
+if [ $pil = 3 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/IqbalzNoobs/fb-brute
+cd fb-brute
+python2 brute.py
+fi
+
+if [ $pil = 4 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/mbf
+cd mbf
+python2 MBF.py
+fi
+
+if [ $pil = 5 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/N1ght420/FaceBrute
+cd FaceBrute
+python fb.py
+fi
+
+if [ $pil = 6 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/thelinuxchoice/facebash.git
+cd facebash
+bash facebash.sh
+fi
+
+if [ $pil = 7 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/EmailVuln
+cd EmailVuln
+python2 vuln.py
+fi
+
+if [ $pil = 8 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/ProfileGuardFb
+cd ProfileGuardFb
+php guard.php
+fi
+
+if [ $pil = 9 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/IlayTamvan/Report
+cd Report
+unzip Report.zip
+python2 Report.py
+fi
+
+if [ $pil = 10 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/AMVengeance/FB-React
+cd FB-React
+./start
+fi
+
+if [ $pil = 11 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/InfoFB
+cd InfoFB
+pip2 install -r requirements.txt
+python2 info.py
+fi
+
+if [ $pil = 12 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+pip2 install mechanize
+git clone https://github.com/Senitopeng/Botkomena.git
+cd Botkomena
+python2 botkomena.py
+fi
+
+if [ $pil = 13 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/autolike
+cd autolike
+php autolike.php
+fi
+
+if [ $pil = 14 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/thelinuxchoice/shellphish.git
+cd shellphish
+bash shellphish.sh
+fi
+
+if [ $pil = 15 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/thelinuxchoice/blackeye.git
+cd blackeye
+bash blackeye.sh
+fi
+
+if [ $pil = 16 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/UndeadSec/SocialFish.git
+cd SocialFish
+chmod +x *
+pip2 install -r requirements.txt
+python2 SocialFish.py
+fi
+
+if [ $pil = 17 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+https://github.com/evait-security/weeman.git
+chmod +x *
+python2 weeman.py
+fi
+
+if [ $pil = 18 ]
+then
+clear
+apt update && apt upgrade
+apt install python2
+pip2 install urllib3 chardet certifi idna requests
+pkg install git
+pip2 install mechanize
+pkg install curl
+pkg install ruby
+pkg install gem
+gem install lolcat
+pkg install git
+pkg install php
+pkg install ruby cowsay toilet figlet
+pkg install neofetch
+pkg install nano
+figlet -f slant " S U K S E S "|lolcat
+fi
+
+if [ $pil = 19 ]
+then
+clear
+figlet -f slant "E X I T"|lolcat
+sleep 2
+echo $i"JANGAN DI"$cy"RECORD"$i"YAH BEB..."
+echo
+sleep 2
+echo $me"█████████"
+echo $me"█▄█████▄█"
+echo $me"█▼▼▼▼▼"
+echo $pu"█" $ku"CODED BY FITRAKUN" $bi"&"$me"SUBSCRIBED CHANNEL ME : JEJAK TUTORIAL ID"
+echo $pu"█▲▲▲▲▲"
+echo $pu"█████████"
+echo $pu"__██____██___"
+sleep 2
+echo $cy"Terima Kasih MR.F1TR4 CYBER DARK TEAM  Saya"
+sleep 2
+echo $i"Bila Ada Kesalahan Kamu Bisa Nanya Kepada Saya"
+sleep 2
+echo
+echo $ku"┈┈┈╲┈┈┈┈╱"$me             "W A : 088262147113"
+echo $ku"┈┈┈╱  ▔▔╲"$me             "F B : Korek Api"
+echo $ku"┈┈┃┈▇┈┈▇┈┃"$pu            "Y T : JEJAK TUTORIAL ID"
+echo $ku"╭╮┣━━━━━━┫╭╮"$pu          "GIT : https://github.com/FITRAKUN404"
+echo $ku"┃┃┃┈┈┈┈┈┈┃┃┃"
+echo $ku"╰╯┃┈┈┈┈┈┈┃╰╯"
+echo $ku"┈┈╰┓┏━━┓┏╯"
+echo $ku"┈┈┈╰╯┈┈╰╯"
+echo
+sleep 2
+echo $pur">> Thanks Yang Sudah Menggunakan Toolz  Saya <<"
+exit
+fi
